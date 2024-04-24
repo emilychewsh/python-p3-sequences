592316c0f3b74776017b4110af54b8991a141d7b
Emily Chew <emilychewsh@gmail.com>
2024-04-24 13:36:21 +1000

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        return print([])
+    elif length == 1 :
+        return print([0])
+    else:
+        fibonacci_list = [0, 1]
+        for i in range(2, length):
+            next_value = fibonacci_list[-1] + fibonacci_list [-2]
+            fibonacci_list.append(next_value)
+        print(fibonacci_list)
+
+print_fibonacci(0)
